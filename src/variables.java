17197f6827b8eb708c95c11fe236f7df070a4af4
shubhamkfa <shubhamkfa@gmail.com>
2024-01-06 16:12:41 +0530

    New

---
+++ src/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+
+    //Local variables
+
+    public static void main(String[]args){
+
+        int a = 5 ;
+
+        System.out.println(a);
+
+    }
+
+}
+
+//Instance Variable
