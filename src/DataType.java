0b300d934032d36e1d7cb22f2d2b99c9a334d957
shubhamkfa <shubhamkfa@gmail.com>
2024-01-06 18:11:16 +0530

    commit all

--- src/DataType.java
+++ src/DataType.java
@@ -8,6 +8,28 @@ public class DataType {
         System.out.println(a);
 
 
+        //double - decimal value can be stored into varialble which is declared with double data type
+
+        double d=77.22345779090765;
+
+        System.out.println (d);
+
+
+        //Char - SIngle character can be stored into a variable which is declared with char data type
+
+        char c= '*';
+
+        System.out.println (c);
+
+
+        //Boolean - True or False value can be stored in boolean data type
+
+        boolean bt= true;
+        boolean bf=false;
+
+        System.out.println (bt);
+        System.out.println(bf);
+
 
 
 
