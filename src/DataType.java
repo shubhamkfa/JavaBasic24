52f47f4faa51a9071fd165c24b710674dc5bc1ab
shubhamkfa <shubhamkfa@gmail.com>
2024-01-06 17:54:48 +0530

    commit all

---
+++ src/DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+
+    public static void main(String[]args){
+
+        //int integer values can be stored into a variable which is declared with int date type
+
+        int a = 223;
+        System.out.println(a);
+
+
+
+
+
+    }
+
+}
---
+++ src/InstanceVariable.java
@@ -0,0 +1,13 @@
+public class InstanceVariable {
+
+    static int a =5; //Instance variable //we have added static as this is a instance variable
+
+    public static void main(String[]args){  //we can remove static from methos also public void main
+
+
+
+        //int a = 5;
+
+        System.out.println(a);
+    }
+}
