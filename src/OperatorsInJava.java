a75e38bf03494481d53b5d9f1dda53a1bca55f2b
shubhamkfa <shubhamkfa@gmail.com>
2024-01-06 18:36:07 +0530

    commit all

---
+++ src/OperatorsInJava.java
@@ -0,0 +1,23 @@
+public class OperatorsInJava {
+
+    public static void main (String[]args){
+        //Operators are just symbols used to perform operations on the provided data.
+
+        //Arithmatic Operators
+
+
+        int a = 17, b=7;
+/* 1. Addition Operator */
+      //system.out.println(a+b);
+
+/* 2.Substraction Operator */
+
+      //System.out.print(b-a);
+//3. Multiplication Operator
+
+        System.out.println(a*b);
+
+
+    }
+
+}
