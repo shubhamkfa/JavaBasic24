4983d96eacc8bb51f39fbb70754ffa6ca34cff14
shubhamkfa <shubhamkfa@gmail.com>
2024-01-06 19:01:44 +0530

    commit all

--- src/OperatorsInJava.java
+++ src/OperatorsInJava.java
@@ -6,18 +6,35 @@ public class OperatorsInJava {
         //Arithmatic Operators
 
 
-        int a = 17, b=7;
-/* 1. Addition Operator */
+       // int a =17 , b=5;
+// 1. Addition Operator +
       //system.out.println(a+b);
 
-/* 2.Substraction Operator */
+// 2.Substraction Operator-
 
       //System.out.print(b-a);
-//3. Multiplication Operator
+//3. Multiplication Operator *
 
-        System.out.println(a*b);
+        //System.out.println(a*b);
 
+//4. Division Operator /
+        //System.out.print(a/b);
+//5.Modulus Operator %
+        //System.out.println(a%b);
+//6.Increment Operator ++
+        int i=10;
 
+//++i; // Pre increment  1 + 10 =11
+        System.out.println(++i);
+        System.out.println(++i);
+        System.out.println(i);
+
+//i++;  //i=10 ie i+a , 10+1 =11 //Post increment
+        //i++;
+
+        System.out.println(i++);
+        System.out.println(i++);
+        System.out.println(i);
     }
 
 }
