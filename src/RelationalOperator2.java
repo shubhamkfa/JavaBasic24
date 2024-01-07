e8092dc3e68aa43d5bbedfe710b6d378dd310dda
shubhamkfa <shubhamkfa@gmail.com>
2024-01-07 23:08:50 +0530

    commit all

---
+++ src/RelationalOperator2.java
@@ -0,0 +1,46 @@
+public class RelationalOperator2 {
+
+    public static void main (String[]args){
+
+       //int a =3,b=23,c=7,d=8;
+
+        //System.out.println(a==b); //False
+       // System.out.println(a!=d);//True
+        //System.out.println(d<b);//True
+       // System.out.println(c>a);//True
+       // System.out.println(a<=c);//True
+        //System.out.println(a>=d);//false
+
+//Logical Operator
+    //&& Logical and operator
+    //||Logical or Operator
+    //! Logical Not
+       // int a=5,b=10;
+       // System.out.println(a>5 ||b <50);
+       // System.out.println(a<c&&b<a);
+        //System.out.println (!(a<c));
+
+//Assignment Operator
+    //=+ Plus equal to //a=5 // a=+4 // a=a+4 //a=5+4 //a=9
+
+        //System.out.println(a+=7);
+        //System.out.println(a-=5);
+        //System.out.println(a*=4);
+        //System.out.println(a/=4);
+        //System.out.println(a%=3);
+       // System.out.println(b&=5);
+
+//Conditional Operator
+        int a=5,b=3,c,d;
+        c=a>b ? 6 : 9;
+        d=a<b ? 2 :7;
+
+        System.out.println(c);
+        System.out.println(d);
+
+
+
+
+
+    }
+}
