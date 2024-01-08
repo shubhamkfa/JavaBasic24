d99c94d66a37a26322284d0ba121b8f04e0eaf0e
shubhamkfa <shubhamkfa@gmail.com>
2024-01-08 10:42:11 +0530

    commit all

---
+++ src/FlowStatement.java
@@ -0,0 +1,30 @@
+public class FlowStatement {
+    //Flow Control statement - Flow control decribes the order in which
+    // statement will be executed in actual run time
+    //1 .Selective statement ( if , if...else , if...else if..else,Switch)
+    //2.Iterative Statement (while,do-while,for ,for-each)
+    //3.Transfer Statement ( break,continue,return,try,catch,finally)
+    public static void main(String[]args){
+
+//Selective Statement If
+//if
+    int a=7, b=10;
+    /*if (a>b) {
+
+        System.out.println("Statement is true");
+    }
+*/
+    if (a<b){
+        System.out.println("A is less than b");
+//if else
+    } else{
+
+        System.out.println("A is greater than b");
+    }
+
+
+
+
+
+    }
+}
