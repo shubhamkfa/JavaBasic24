29f4060984de1e439a93368b27d4a8e54357f5f5
shubhamkfa <shubhamkfa@gmail.com>
2024-01-09 19:33:57 +0530

    commit all

--- src/ConditionalIfelseSwitch.java
+++ src/ConditionalIfelseSwitch.java
@@ -17,15 +17,63 @@ public class ConditionalIfelseSwitch {
 
 */
 
-
+/*
         Scanner sc=new Scanner (System.in);
         int n= sc.nextInt();
 
         if (n%2==0){
-            System.out.println("even");
+            System.out.println("even Number");
+        }else{
+
+            System.out.println("Odd Number");
         }
 
+*/
+       // Scanner sc=new Scanner(System.in);
+       // int a=sc.nextInt();
+       // int b=sc.nextInt();
+/*
+        if (a==b){
+            System.out.println("Equal");
+        }else{
+            if (a>b) {
+                System.out.println("A is greater");
+            }else{
+                System.out.println("A is lesser");
+            }
 
+        }
+*/ /*
+        if (a==b){
+            System.out.println("Equal");
+
+        }else if (a>b){
+            System.out.println("A is greter");
+        }
+        else{
+            System.out.println("A is lesser");
+        }
+*/
+       // if (3%2 ==0) {
+        //    System.out.println("Bazinga");
+        //}
+
+
+        //Problem1
+
+        Scanner sc=new Scanner (System.in);
+        int button=sc.nextInt();
+
+        if (button==1){
+            System.out.println("Hello");
+        }else if (button==2){
+            System.out.println("Namaste");
+        }else if (button==3){
+            System.out.println("Bonjour");
+
+        }else {
+            System.out.println("Invilid Button");
+        }
 
 
 
