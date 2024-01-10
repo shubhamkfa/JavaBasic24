bd8384edd5d99fc42c64a4610544a6d7d87a9daa
shubhamkfa <shubhamkfa@gmail.com>
2024-01-10 17:25:53 +0530

    commit all

---
+++ src/IterativeStatementWhileloop.java
@@ -0,0 +1,16 @@
+public class IterativeStatementWhileloop {
+    public static void main(String[]args){
+
+        int a =5;
+        int b=7;
+
+        while (a<b){
+            System.out.println("A is less than B");
+
+
+
+        }
+
+
+    }
+}
