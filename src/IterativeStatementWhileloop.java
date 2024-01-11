3219cadaed2ffec38925ccbfbb9d2ad929bcbe5e
shubhamkfa <shubhamkfa@gmail.com>
2024-01-11 18:41:42 +0530

    commit all

--- src/IterativeStatementWhileloop.java
+++ src/IterativeStatementWhileloop.java
@@ -7,10 +7,21 @@ public class IterativeStatementWhileloop {
 //            System.out.println("A is less than B");
 //        }
         // for
-        for (int counter = 0; counter < 3; counter = counter + 1){
-            System.out.println("For Loop Three Times");
+//        for (int counter = 0; counter < 10; counter = counter + 1){
+//            System.out.println("For Loop Three Times");
+        //counter++->counter->counter+1
+//            for (int counter = 0; counter < 11; counter++){
+//                System.out.println(counter+"");
+//
+//        }
+//            for (int i=0;i<12;i++){
+//                System.out.println(i+" ");
+//            }
+
+        int a =5,b=8;
+        while (a<b){
+            System.out.println("True STatement");
+
         }
-
-
     }
 }
