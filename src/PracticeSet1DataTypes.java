244cc3f2e222465433cdd69658ba809a6f84328c
shubhamkfa <shubhamkfa@gmail.com>
2024-01-12 16:59:15 +0530

    commit all

---
+++ src/PracticeSet1DataTypes.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class PracticeSet1DataTypes {
+
+
+    public static void main(String[] args) {
+
+        //Examples from https://toolsqa.com/java/data-types-variables/
+        //1. Write a Java program to print "Hello World" on the screen.
+        //Scanner sc = new Scanner (System.in);
+        //int a = sc.nextInt();
+        //String s= sc.next();
+        //System.out.println(s+ " ");
+
+        //System.out.println("Hello World");
+        //System.out.println('a');
+
+        // 2. Write a Java program to print the following output:
+
+//        System.out.println("@@@@@@");
+//        System.out.println("++++++");
+//        System.out.println("*****");
+//        System.out.println("######");
+
+        //3. Write a Java program to declare three variable
+        // a, b, & c and store the values respectively 10, 20.3 & 3.14785 .
+        // Then display their values on the screen.
+
+
+//        int a=10;
+//        double b= 20.3;
+//        double c=3.14785;
+//
+//        System.out.println("The value of a is : "+ a);
+//        System.out.println("The value of b is : "+ b);
+//        System.out.println("The value of c is : "+ c);
+
+
+//       Example 4. Write a Java program to declare
+//        a Boolean variable with initial value of "true"
+//        and later change it to "false" before printing it.
+
+        Boolean a = true;
+
+        a=false;
+
+        System.out.println("The boolean value of variable a is : " + a);
+
+
+//        Scanner sc= new Scanner (System.in);
+//        int g = sc.nextInt();
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
