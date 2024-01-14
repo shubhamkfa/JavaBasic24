79654a6a98d7fa72dd184a1891c8d5746235023f
shubhamkfa <shubhamkfa@gmail.com>
2024-01-14 18:25:05 +0530

    commit all

--- src/IterativeStatementWhileloop.java
+++ src/IterativeStatementWhileloop.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class IterativeStatementWhileloop {
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
 //        int a =5;
 //        int b=7;
@@ -31,7 +31,7 @@ public class IterativeStatementWhileloop {
 //            i=i+1;
 //        }while(i<10);
 
-
+/*
        // Que-1  #print thw Sum of n Natural Number
         Scanner sc= new Scanner (System.in);
         int n = sc.nextInt();
@@ -41,8 +41,74 @@ public class IterativeStatementWhileloop {
             sum=sum + 1;
 
         } System.out.print(sum);
+*/
 
+//        for (int i=0; i<=9; i++ ) {
+//            System.out.print(i+" ");
+
+        /*WHile loop
+            int i=0;
+            while (i<11){
+
+                //i++;
+                System.out.println(i);
+                i = i+1;
+            }
+*/
+        /* Do While
+
+        int i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i<11);
+    }
+
+
+         */
+/*
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+
+
+
+        for (int i=1;  i<= 10; i++) {
+            System.out.println(n*i);
+
+//            sum=n*i;
+//            System.out.println(n + "*" + i + "=" + sum);
+
+//Homework Apana colleage
+//1.Print all even numbers till n.
+// */   /*  Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+        //int sum=n==0;
+        int n=25;
+
+        for(int i=1; i<=n; i++){
+            if
+
+            System.out.println(n);
+
+
+        }
+
+        */
+
+
+        //For Loop By Arun
+
+        //For loop is mostly popular and useful
+
+        for (int i=1;i<6;i++){
+
+            System.out.println(i);
+        }
 
 
     }
 }
+
+
+
