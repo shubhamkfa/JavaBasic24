e705727df46d119590d6a3c3b8604b2fbe05d94f
shubhamkfa <shubhamkfa@gmail.com>
2024-01-14 22:00:46 +0530

    commit all

--- src/PracticSet2_Pattern.java
+++ src/PracticSet2_Pattern.java
@@ -27,14 +27,45 @@ public class PracticSet2_Pattern {
         }
 
   */
-        //Question 2-Print Hollow Rectangle
+/*        //Question 2-Print Hollow Rectangle
 
+        int n=4;
+        int m=5;
 
+        for (int i=1; i<=n; i++){
+            for( int j=1; j<=m; j++) {
+                if (i == 1 || j==1 || i==n || j==m){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
 
+            }System.out.println();
 
+        }
+*/
 
+/* Half Pyramin
 
+        int n=4;
 
+        for(int i=1;i<=n; i++){
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+*/
+
+    //Inverted Half Pyramid
+
+        int n=4;
+        for(int i=n; i>=1; i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
 
 
 
