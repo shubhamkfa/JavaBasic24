b0a2581537db7cb63ff7717273e24d59fb35c424
shubhamkfa <shubhamkfa@gmail.com>
2024-01-16 16:57:04 +0530

    commit all

---
+++ src/Methods_97.java
@@ -0,0 +1,12 @@
+public class Methods_97 {
+
+    public static void main(String[]args){
+        int a=1,b=5,c=19;
+
+        c=b-a;
+
+        System.out.println(c + " Is the answer ");
+
+
+    }
+}
