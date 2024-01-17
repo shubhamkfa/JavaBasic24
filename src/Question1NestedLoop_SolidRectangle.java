57d6e43fe19e5cf92157a5eda93ed86e37ed3c76
shubhamkfa <shubhamkfa@gmail.com>
2024-01-17 19:25:09 +0530

    commit all

--- src/Question1NestedLoop_SolidRectangle.java
+++ src/Question1NestedLoop_SolidRectangle.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Question1NestedLoop_SolidRectangle {
     public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
-    int n=sc.nextInt();
-    int m=sc.nextInt();
+    int n=sc.nextInt(); //n=4
+    int m=sc.nextInt(); //m=5
 
 
         for(int i=1;i<=n;i++){
