86504993b3e7164c3a5c7c6d7d833cb9be9006a2
shubhamkfa <shubhamkfa@gmail.com>
2024-01-17 19:18:20 +0530

    commit all

---
+++ src/Question1NestedLoop_SolidRectangle.java
@@ -0,0 +1,14 @@
+public class Question1NestedLoop_SolidRectangle {
+//Nested Loop - Print Pattern Solid Rectangles 9/10
+    //*****
+    public static void main(String[]args){
+        for(int i=1;i<=9;i++){
+
+            for(int j=1;j<=10;j++){
+                System.out.print("*"+" ");
+            }System.out.println();
+        }
+
+
+    }
+}
