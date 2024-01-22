15f0007dbcf4d14cdb358e8a68f33d5e20072729
shubhamkfa <shubhamkfa@gmail.com>
2024-01-22 19:14:36 +0530

    commit all

--- src/Que3_HalfPyramid.java
+++ src/Que3_HalfPyramid.java
@@ -4,14 +4,29 @@ public class Que3_HalfPyramid {
 
         int n=5;
 
-        for (int i=1;i<=n;i++){
+        for (int i=1;i<=n;i++) {
 
-            for(int j=1;j<=i;j++){
+            for (int j = 1; j <= i; j++) {
 
                 System.out.print(" * ");
-            }System.out.println();
-
+            }
+            System.out.println();
         }
+        //print 1- 100 number
+//        for(int i=1;i<=100;i++){
+//
+//            System.out.println(i);
+
+        //Print Prime Number
+
+//        int n=100;
+//
+//        for (int i=1; i<=n; i++ ){
+//            if i
+//
+//            System.out.println(i);
+//
+//        }
 
 
 
